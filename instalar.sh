93d5a9a2e5b7ba24032bc6dc84e9b24bf08e3a30
justme088 <39110970+justme088@users.noreply.github.com>
2023-10-12 02:09:19 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -2,6 +2,6 @@
 sudo mkdir /opt/proyecto_htp02/
 sudo chmod 777 /opt/proyecto_htp02/
 wget https://rb.gy/32yvk -O agfis101.tar.gz 
-mv AG_fis101.tar.gz /opt/proyecto_htp02/agfis101.tar.gz
+mv agfis101.tar.gz /opt/proyecto_htp02/agfis101.tar.gz
 tar -xzvpf /opt/proyecto_htp02/agfis101.tar.gz -C /opt/proyecto_htp02/
 rm /opt/proyecto_htp02/agfis101.tar.gz
