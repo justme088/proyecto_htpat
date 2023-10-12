71a73d954819054d58d75130c1123960c71bc39c
justme088 <39110970+justme088@users.noreply.github.com>
2023-10-11 21:06:58 -0500

    Add files via upload

---
+++ instalar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mkdir /opt/proyecto_fis101
+sudo chmod 777 /opt/proyecto_fis101
+wget https://rb.gy/y8oiz -O AG_fis101.tar.gz 
+mv AG_fis101.tar.gz /opt/proyecto_fis101/AG_fis101.tar.gz
+tar -xzvpf /opt/proyecto_fis101/AG_fis101.tar.gz
+rm /opt/proyecto_fis101/AG_fis101.tar.gz
