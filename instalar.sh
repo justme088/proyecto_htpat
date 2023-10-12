a022c68ecb667c71c406ecb5c8a8288d8463023d
justme088 <39110970+justme088@users.noreply.github.com>
2023-10-12 02:05:18 -0500

    Update instalar.sh

--- instalar.sh
+++ instalar.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-sudo mkdir /opt/proyecto_fis101
-sudo chmod 777 /opt/proyecto_fis101
-wget https://rb.gy/82knu -O AG_fis101.tar.gz 
-mv AG_fis101.tar.gz /opt/proyecto_fis101/AG_fis101.tar.gz
-tar -xzvpf /opt/proyecto_fis101/AG_fis101.tar.gz -C /opt/proyecto_fis101/
-rm /opt/proyecto_fis101/AG_fis101.tar.gz
+sudo mkdir /opt/proyecto_htp02/
+sudo chmod 777 /opt/proyecto_htp02/
+wget https://rb.gy/32yvk -O agfis101.tar.gz 
+mv AG_fis101.tar.gz /opt/proyecto_htp02/agfis101.tar.gz
+tar -xzvpf /opt/proyecto_htp02/agfis101.tar.gz -C /opt/proyecto_htp02/
+rm /opt/proyecto_htp02/agfis101.tar.gz
