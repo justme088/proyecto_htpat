c31fbd0125d822e5640c1af2b6175462e28d9617
justme088 <39110970+justme088@users.noreply.github.com>
2023-10-11 21:00:58 -0500

    Delete instalar.sh

--- instalar.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-sudo mkdir /opt/proyecto_fis101
-sudo chmod 777 /opt/proyecto_fis101
-wget https://rb.gy/y8oiz -O AG_fis101.tar.gz -P /opt/proyecto_fis101
-tar -xzvpf /opt/proyecto_fis101/AG_fis101.tar.gz
-rm /opt/proyecto_fis101/AG_fis101.tar.gz
