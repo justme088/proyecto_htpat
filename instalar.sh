f1a68f3ca4a2b7cfe45b1f6ee777a9614a05ef01
justme088 <39110970+justme088@users.noreply.github.com>
2023-10-16 08:03:53 -0500

    Add files via upload

---
+++ instalar.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo mkdir /opt/proyecto_htp02/
+sudo chmod 777 /opt/proyecto_htp02/
+wget https://rb.gy/sb03r -O agfis101.tar.gz 
+mv agfis101.tar.gz /opt/proyecto_htp02/agfis101.tar.gz
+tar -xzvpf /opt/proyecto_htp02/agfis101.tar.gz -C /opt/proyecto_htp02/
+rm /opt/proyecto_htp02/agfis101.tar.gz
